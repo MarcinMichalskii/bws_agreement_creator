b889b0e87a4c6fa46311a4bff48e04ee6daf7946
Marcin Michalski <m4r.michalski@gmail.com>
2023-01-05 19:05:36 +0100

    test

--- lib/utils/byte_data_extension.dart
+++ lib/utils/byte_data_extension.dart
@@ -2,6 +2,8 @@ import 'package:flutter/foundation.dart';
 
 extension ByteDataToUint8 on ByteData {
   Uint8List dataAsUint8() {
+    print('offset: $offsetInBytes');
+    print('offset: $lengthInBytes');
     return buffer.asUint8List(offsetInBytes, lengthInBytes);
   }
 }
