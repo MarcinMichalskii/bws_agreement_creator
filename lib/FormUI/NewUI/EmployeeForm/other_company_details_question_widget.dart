c01979dd7a13d64e0c193451408e46a2bd9aeff6
Marcin Michalski <marcin.michalski@finanteq.com>
2024-02-11 20:59:49 +0100

    update nip

--- lib/FormUI/NewUI/EmployeeForm/other_company_details_question_widget.dart
+++ lib/FormUI/NewUI/EmployeeForm/other_company_details_question_widget.dart
@@ -5,6 +5,7 @@ import 'package:bws_agreement_creator/FormUI/components/bordered_input.dart';
 import 'package:bws_agreement_creator/FormUI/components/form_toggle.dart';
 import 'package:bws_agreement_creator/FormUI/components/select_date_button.dart';
 import 'package:bws_agreement_creator/utils/colors.dart';
+import 'package:bws_agreement_creator/utils/nip_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -48,6 +49,7 @@ class OtherCompanyDetailsQuestionWidget extends HookConsumerWidget {
       ),
       BorderedInput(
           placeholder: "NIP firmy",
+          validator: NipValidator.validate,
           onChanged: (value) {
             ref
                 .read(newFormDataProvider.notifier)
