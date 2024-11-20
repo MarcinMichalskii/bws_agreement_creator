1ae56b41f24daed5deed5e64648fc65423e8f949
Marcin Michalski <marcin.michalski@finanteq.com>
2024-11-20 15:44:33 +0100

    remove .env

--- lib/utils/base_url.dart
+++ lib/utils/base_url.dart
@@ -1,3 +1,7 @@
-const baseUrl = 'https://bws-agreement-creator-s2y2v.ondigitalocean.app';
+import 'package:bws_agreement_creator/app_config_helper.dart';
+
+var baseUrl = isProd
+    ? 'https://bws-agreement-creator-s2y2v.ondigitalocean.app'
+    : 'https://dev-agreement-creator-629fh.ondigitalocean.app';
 // const baseUrl = 'https://dev-agreement-creator-629fh.ondigitalocean.app';
 // const baseUrl = 'http://localhost:8080';
--- lib/utils/no_access_token_paths.dart
+++ lib/utils/no_access_token_paths.dart
@@ -1,7 +1,7 @@
 import 'package:bws_agreement_creator/utils/base_url.dart';
 
 class NoAccessTokenPaths {
-  static const List<String> paths = [
+  static List<String> paths = [
     '$baseUrl/refreshToken',
     '$baseUrl/login',
     '$baseUrl/loginWithGoogle'
