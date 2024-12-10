b1ca0d3a1a43901ab79913ab786e9d2ed723a418
Marcin Michalski <marcin.michalski@finanteq.com>
2024-12-10 23:52:53 +0100

    Update base url

--- lib/utils/base_url.dart
+++ lib/utils/base_url.dart
@@ -1,7 +1,7 @@
 import 'package:bws_agreement_creator/app_config_helper.dart';
 
-// var baseUrl = isProd
-// ? 'https://bws-agreement-creator-s2y2v.ondigitalocean.app'
-// : 'https://dev-agreement-creator-629fh.ondigitalocean.app';
+var baseUrl = isProd
+    ? 'https://bws-agreement-creator-s2y2v.ondigitalocean.app'
+    : 'https://dev-agreement-creator-629fh.ondigitalocean.app';
 // const baseUrl = 'https://dev-agreement-creator-629fh.ondigitalocean.app';
-const baseUrl = 'http://localhost:8080';
+// const baseUrl = 'http://localhost:8080';
